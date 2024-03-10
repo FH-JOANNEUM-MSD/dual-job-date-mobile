0d082f39870657e4747d0b95e319c8fb4d23c5c6
jalal-CodeProducer <jalal.codeproducer@gmail.com>
2024-03-10 15:05:00 +0100

    working on icons

--- lib/widgets/bottom_nav_bar.dart
+++ lib/widgets/bottom_nav_bar.dart
@@ -1,6 +1,5 @@
 import 'package:dual_job_date_mobile/static_helpers/colors.dart';
 import 'package:dual_job_date_mobile/static_helpers/strings.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class BottomNavBar extends StatelessWidget {
