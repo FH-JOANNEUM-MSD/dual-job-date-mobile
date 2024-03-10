985b3cdf1e8fa79e9129a3437f00bbf73d6f90df
TheRealFranzmann <franz.lafer@edu.fh-joanneum.at>
2024-03-10 14:09:59 +0100

    Implemented forgot password screen and created custom widgets for it
    Textinputs and Button for Forgot Password Screen. #WIP
    
    Design for textinputs
    
    Circular Textinputs, no Borders, looks beautiful, would recommend 10 out of 10
    
    Added Logo
    
    Logo added on top of screen
    
    Created a custom TextFormField for less verbose code
    
    added comments
    
    Made CustomTextFormField a widget
    
    renamed files and branch
    
    changed syntax of CustomTextFormField
    
    commit for merge
    
    added missing asset
    
    moved all hardcoded values to constants, created custom button Widget, comments
    
    adhering to naming convention and folder structure
    
    Forgot Password Screen
    
    Added the forgot password screen UI without logic
    
    added Colors and fixed white bar
    
    Added new static Colors for the Background and fixed the issue with the white bar on the bottom of the screen
    
    extracted padding to make custom widget
    
    removed useless attribute
    
    Use custom padding widget, improved Comments
    
    added custom padding to this screen to make code consistent

---
+++ lib/screens/forgot_password.dart
@@ -0,0 +1,90 @@
+import 'package:dual_job_date_mobile/static_helpers/colors.dart';
+import 'package:dual_job_date_mobile/widgets/custom_form_padding.dart';
+import 'package:dual_job_date_mobile/widgets/custom_text_form_field.dart';
+import 'package:flutter/material.dart';
+
+class ForgotPassword extends StatefulWidget {
+  const ForgotPassword({super.key});
+
+  @override
+  State<ForgotPassword> createState() => _ForgotPasswordState();
+}
+
+class _ForgotPasswordState extends State<ForgotPassword> {
+  final TextEditingController _emailController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    super.dispose();
+  }
+
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        body: SafeArea(
+          child: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  StaticColors.topBackgroundScreen,
+                  StaticColors.bottomBackgroundScreen,
+                ],
+              ),
+            ),
+
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Image.asset(
+                  StaticStrings.logo,
+                  height: Values.logoImageWidthHeight_200,
+                  width: Values.logoImageWidthHeight_200,
+                ),
+                const Text(
+                  StaticStrings.resetPassword,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: Values.headerTextSize_24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                CustomFormPadding(
+                  childWidget: CustomTextFormField(controller:  _emailController,
+                  hintText: CustomTextFormField.emailText),
+                ),
+                CustomFormPadding(
+                  childWidget: CustomElevatedButton(
+                    text: CustomElevatedButton.sendButtonText,
+                    onPressed: (){
+                    //TODO: implement me....
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                  backgroundColor: Colors.green,
+                  textStyle: const TextStyle(color: Colors.white),
+                  minimumSize: const Size(double.infinity, 55),
+                ),
+                child: const Text(
+                  'SPEICHERN',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+                  ),
+                ),
+            ),
+          );
+  }
+}
+
+// FIXME: Just for testing will be removed afterwards
+void main() {
+  runApp(MaterialApp(home: ForgotPassword()));
+}
--- lib/static_helpers/strings.dart
+++ lib/static_helpers/strings.dart
@@ -1,7 +1,11 @@
 class StaticStrings{
-  static const String logo = "assets/images/logo.png";
-  static const String calendar = "assets/images/calendar_icon.png";
-  static const String flame = "assets/images/flame_icon.png";
-  static const String group = "assets/images/group_icon.png";
-  static const String profile = "assets/images/profile_icon.png";
+  //Paths
+  static const String logo = 'assets/images/logo.png';
+  static const String calendar = 'assets/images/calendar_icon.png';
+  static const String flame = 'assets/images/flame_icon.png';
+  static const String group = 'assets/images/group_icon.png';
+  static const String profile = 'assets/images/profile_icon.png';
+  //Text
+  static const changePassword = 'Passwort ändern';
+  static const resetPassword = 'Passwort zurücksetzen';
 }
