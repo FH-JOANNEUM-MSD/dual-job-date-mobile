728be6a47869546b35d5e3e9739bbd4b46e7cf06
lukasbeckercode <lukas.becker.lb@outlook.com>
2024-03-10 17:59:29 +0100

    added comments

--- lib/custom_widgets/CustomTextFormField.dart
+++ lib/custom_widgets/CustomTextFormField.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 
+/// Class Representing a TextFormField as used in the App with all customizations according to UI/UX in place
 class CustomTextFormField {
-  late TextFormField field ;
+  late TextFormField field ; // The field that gets returned for use
+
+  //Hint Text constants
   static const  String newPasswordText = 'Neues Passwort';
   static const  String repeatNewPasswordText = 'Passwort Wiederholen';
   static const  String currentPasswordText = 'Aktuelles Passwort';
 
+  /// Constructor for the Text form field
+  /// controller: A TextEditingController for the CustomTextFormField
+  /// hintText:  A String used as a hintText in the CustomTextFormFields
+  /// use the constants as a hint text for best practise.
   CustomTextFormField(TextEditingController controller, String hintText){
+    //Create the field
+
     field = TextFormField(
       textAlign: TextAlign.center,
       controller: controller,
@@ -32,6 +41,7 @@ class CustomTextFormField {
     );
   }
 
+  /// get the custom widget back (type: TextFormField). Can be used directly.
   TextFormField getWidget() => field;
 
 }
