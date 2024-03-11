f8ecc26b49d9a9eccf976e322c4cfd7c79f64406
Mako CP <mako.codeproducer@gmail.com>
2024-03-11 22:15:57 +0100

    companies screen setup

--- lib/tabs/companies.dart
+++ lib/tabs/companies.dart
@@ -1,4 +1,9 @@
+import 'package:dual_job_date_mobile/static_helpers/colors.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import '../components/companyCard.dart';
 
 class Companies extends StatefulWidget {
   const Companies({super.key});
@@ -10,11 +15,23 @@ class Companies extends StatefulWidget {
 class _CompaniesState extends State<Companies> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: DefaultTextStyle(
-        style: TextStyle(color: Colors.black),
-        child: Text("Companies"),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Unternehmen',
+                style: TextStyle(fontSize: 24.0),
+              ),
+              Image.asset('assets/images/filter_icon.png')],
+          ),
+        ),
+        body: ListView.builder(
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return const CompanyCardWidget();
+            }));
   }
 }
