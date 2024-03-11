0b878248cfb37170232da910dfb3f706db8d2a46
Mako CP <mako.codeproducer@gmail.com>
2024-03-11 22:16:24 +0100

    add additional colors

--- lib/static_helpers/colors.dart
+++ lib/static_helpers/colors.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 class StaticColors {
-  static const Color primary = Color(0xff53851E);
-  static const Color secondary = Color(0xff73B72A);
-  static const Color shadow = Color(0xffCDECAC);
-
+  static const Color primary = Color(0xFF53851E);
+  static const Color secondary = Color(0xFF73B72A);
+  static const Color white = Color(0xFFF2F2F3);
+  static const Color black = Color(0xFF000000);
+  static const Color shadow = Color(0xFFCDECAC);
 }
