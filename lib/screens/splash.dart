f06e68d3e5d919dcbd5bd91ea2562300d9010ead
Kathrin Hubmann <kathrin.hubmann@edu.fh-joanneum.at>
2024-03-14 21:03:53 +0100

    Made splash.dart a stateful widget so it can be used as a splash screen

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,21 +1,52 @@
+import 'dart:async';
+import 'package:dual_job_date_mobile/screens/login.dart';
 import 'package:flutter/material.dart';
 
-class Splash extends StatelessWidget {
+class Splash extends StatefulWidget {
   const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<Splash> {
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        const Duration(seconds: 3),
+            () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => const Login())));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: const BoxDecoration(
-        color: Color(0xFF5F8923),
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: <Color> [
+            Color(0xFFF2FFE4),
+            Color(0xFF73B72A)
+          ]
+        ),
       ),
-      child: Column(
+      child: Stack(
         children: [
-          const SizedBox(height: 100),
-          Image.asset('assets/images/logo.png'),
-          const DefaultTextStyle(
-              style: TextStyle(color: Colors.white, fontSize: 40),
-              child: Text("DUAL DATING")
+          Padding(
+            padding: const EdgeInsets.only(top: 80),
+            child: Image.asset('assets/images/logo.png'),
+          ),
+          const Center(
+            child: Padding(
+              padding: EdgeInsets.only(top: 40),
+              child: DefaultTextStyle(
+                  style: TextStyle(color: Colors.white, fontSize: 40),
+                  child: Text("DUAL DATING")
+              ),
+            ),
           ),
         ]
       ),
