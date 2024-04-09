405656644adf9fa3bb498347aaa8e17ecc6e2053
Elisabeth <elisabeth.wadler@edu.fh-joanneum.at>
2024-04-09 15:42:25 +0200

    refactor: show error on timeout + merge development into branch

--- lib/rest/APIservice.dart
+++ lib/rest/APIservice.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:developer';
+import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
@@ -8,52 +9,64 @@ class APIService {
   static final storage = FlutterSecureStorage();
 
   static Future<bool> login(String email, String password) async {
-    final response = await http.post(
-      Uri.parse('$baseUrl/User/Login'),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: jsonEncode({
-        'email': email,
-        'password': password,
-      }),
-    );
+    try {
+      final response = await http
+          .post(
+            Uri.parse('$baseUrl/User/Login'),
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: jsonEncode({
+              'email': email,
+              'password': password,
+            }),
+          )
+          .timeout(const Duration(seconds: 2));
 
-    if (response.statusCode == 200) {
-      Map<String, dynamic> decodedResponse = jsonDecode(response.body);
-      await storage.write(key: 'token', value: decodedResponse['accessToken']);
-      return true;
-    } else {
+      if (response.statusCode == 200) {
+        Map<String, dynamic> decodedResponse = jsonDecode(response.body);
+        await storage.write(
+            key: 'token', value: decodedResponse['accessToken']);
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      log(e.toString());
       return false;
     }
   }
-  static Future<bool> setNewPassword(String oldPassword, String newPassword) async {
+
+  static Future<bool> setNewPassword(
+      String oldPassword, String newPassword) async {
     // Read the token from secure storage
     String? token = await storage.read(key: 'token');
 
     if (token == null) {
-      print('Not Authorized!');
       return false;
     }
-    log(token);
-    log(oldPassword);
-    log(newPassword);
 
-    final response = await http.post(
-      Uri.parse('$baseUrl/User/ChangePassword'),
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer $token',
-      },
-      body: jsonEncode({
-        'oldPassword': oldPassword,
-        'newPassword': newPassword,
-      }),
-    );
+    try {
+      final response = await http
+          .post(
+            Uri.parse('$baseUrl/User/ChangePassword'),
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': 'Bearer $token',
+            },
+            body: jsonEncode({
+              'oldPassword': oldPassword,
+              'newPassword': newPassword,
+            }),
+          )
+          .timeout(const Duration(seconds: 2));
 
-    if (response.statusCode == 200) {
-      return true;
-    } else {
+      if (response.statusCode == 200) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
       return false;
     }
   }
