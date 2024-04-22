b54a41588445086591c4166a9b97f08a0fe26122
Mako CP <mako.codeproducer@gmail.com>
2024-04-22 21:33:13 +0200

    fix loading state companies

--- lib/tabs/companies/companies_bloc.dart
+++ lib/tabs/companies/companies_bloc.dart
@@ -7,15 +7,13 @@ import 'companies_state.dart';
 class CompaniesBloc extends Bloc<CompaniesEvent, CompaniesState> {
   CompaniesBloc() : super(CompaniesStateInitial()) {
     on<CompaniesFetchEvent>((event, emit) async {
-      if (event == CompaniesFetchEvent()) {
-        emit(CompaniesStateLoading());
+      emit(CompaniesStateLoading());
 
-        var companies = await CompanyService.getActiveCompanies();
-        if (companies != null) {
-          emit(CompaniesStateLoaded(companies));
-        } else {
-          emit(CompaniesStateError());
-        }
+      var companies = await CompanyService.getActiveCompanies();
+      if (companies != null) {
+        emit(CompaniesStateLoaded(companies));
+      } else {
+        emit(CompaniesStateError());
       }
     });
   }
