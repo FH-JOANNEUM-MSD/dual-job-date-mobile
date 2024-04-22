de2d8d45286c5fe24c41c480c515c8c3ffdf5856
Mako CP <mako.codeproducer@gmail.com>
2024-04-22 16:31:32 +0200

    remove likesandmatches screen (no more necessary)

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,7 +2,6 @@ import 'package:dual_job_date_mobile/tabs/appointments.dart';
 import 'package:dual_job_date_mobile/tabs/companies.dart';
 import 'package:dual_job_date_mobile/tabs/profile.dart';
 import 'package:dual_job_date_mobile/tabs/more.dart';
-import 'package:dual_job_date_mobile/tabs/view_likes_and_matches.dart';
 import 'package:dual_job_date_mobile/widgets/custom_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -18,7 +17,6 @@ class _HomeState extends State<Home> {
   late int currentIndex;
   List<Widget> tabs = [
     const Companies(),
-    const LikesAndMatches(),
     const Appointments(),
     const Profile(),
     const More()
